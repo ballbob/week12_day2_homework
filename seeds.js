60533862b7d721decbd8c7d07d6ae55d8f30e953
Robert Ball <robert.ball.1989@gmail.com>
2017-04-18 15:37:09 +0100

    Seeds, server and package configured. The program will contain a database of arachnids with some documents of orders.

---
+++ seeds.js
@@ -0,0 +1,35 @@
+use arachnida
+
+db.arachnida.insert([
+
+  {
+    name: "Whip scorpion",
+    order: "Thelyphonida"
+  },
+
+  {
+    name: "Whip spider",
+    order: "Amblypygi"
+  },
+
+  {
+    name: "Spider",
+    order: "Araneae"
+  },
+
+  {
+    name: "Scorpion",
+    order: "Scorpiones"
+  },
+
+  {
+    name: "Solifuge",
+    order: "Solifugae"
+  },
+
+  {
+    name: "Mites and ticks",
+    order: "Acari"
+  }
+
+  ])
